1c498be8c8ff00230898177a1d6b923cd7e2a95e
Ihor Ponarin <ihor_ponarin@epam.com>
2022-06-03 18:52:06 +0300

    Initial commit

---
+++ src/main/java/ua/ponarin/triviausers/TriviaUsersApplication.java
@@ -0,0 +1,13 @@
+package ua.ponarin.triviausers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaUsersApplication.class, args);
+    }
+
+}
